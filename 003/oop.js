5bbc79f084f1f09cb5c194ee46b11a901f85accb
UnitR330 <145355731+UnitR330@users.noreply.github.com>
2023-12-04 20:12:17 +0200

    Update

--- 003/oop.js
+++ 003/oop.js
@@ -30,5 +30,97 @@ matrix.show();
 
 console.log(terminator, matrix);
 
-const box = {};
+class Box {
+    constructor() {
+        this.item = ''
+    }
+    put(what) {
+        this.item = what;
+    }
+    emptyBox() {
+        this.item = '';
+    }
+};
 
+const box1 = new Box();
+const box2 = new Box();
+
+
+box1.put('pen');
+box2.put('pencil');
+box1.put('eraser');
+
+box1.emptyBox();
+
+console.log(box1, box2);
+
+// 2
+
+class RandomDigit {
+    constructor() {
+      this.generateRandomDigit();
+    }
+  
+    generateRandomDigit() {
+      this.digit = Math.floor(Math.random() * 10);
+    }
+  }
+  
+  const randomDigitInstance = new RandomDigit();
+  console.log("Random Digit:", randomDigitInstance.digit);
+//2*
+class RandomNumber {
+    constructor() {
+        this.digit = this.rand(0, 9);
+    }
+    rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max - min + 1) +1);
+}
+}
+
+const digit1 = new RandomDigit();
+const digit2 = new RandomDigit();
+
+console.log(digit1, digit2);
+//3 
+class RandomDigit2 {
+    constructor() {
+      this.generateRandom();
+    }
+  
+    generateRandom() {
+      this.digit = Math.floor(Math.random() * 10);
+    }
+  }
+  
+  const randomDigitArray = Array.from({ length: 10 }, () => new RandomDigit());
+  
+  console.log("Generated Numbers:");
+  randomDigitArray.forEach((randomDigit, index) => {
+    console.log(`Digit ${index + 1}: ${randomDigit.digit}`);
+  });
+  
+
+  //3*
+
+  const arr = new Array(10)
+  .fill(null)
+  .map(_ => new RandomDigit())
+  .map(el => console.log(el.digit) || el.digit);
+
+  console.log(arr);
+
+  //3 **
+/*
+  const _ = require('lodash');
+  const length = 10;
+  const min = 0;
+  const max = 10;
+  const numbers = [...Array(length)]; 
+  for (let i = 0; i < numbers.length; i += 1) {    
+    numbers[i] = _.random(min, max);   }
+
+    console.log(numbers); 
+    */
