931950e6dd7721d21cf7a78fdb07c0ec362ac185
UnitR330 <145355731+UnitR330@users.noreply.github.com>
2023-12-04 17:56:36 +0200

    Update

--- 003/oop.js
+++ 003/oop.js
@@ -2,12 +2,22 @@ console.log('This is Object Oriented Programming');
 
 class Movie {
 
-    
+    constructor(title) {
+        this.title = title;
+        console.log('Movie constructor');
+    }
+
+    show() {
+        console.log('Show movie' + this.title);
+    }
 
 }
 
-const terminator = new Movie();
+const terminator = new Movie('Terminator');
+const matrix = new Movie('Matrix')
+
 const terminatorWhat = {};
+
 
 const arr1 = []; //short hand for new Array();
 const arr2 = new Array();
@@ -15,4 +25,10 @@ const arr2 = new Array();
 const array1 = [];
 const array2 = {}; 
 
-console.log(terminator);
+terminator.show();
+matrix.show();
+
+console.log(terminator, matrix);
+
+const box = {};
+
